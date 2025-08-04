a50be85bc7820433b1ecdcaffad8663654b51aad
Pete118bull <pete118@aol.com>
2025-08-04 20:40:03 +0200

    revised intro text3

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
     <main style={{ padding: "2rem", maxWidth: "800px", margin: "0 auto" }}>
       <h1>NHS OKR Assistant</h1>
       <p style={{ marginBottom: "1rem" }}>
-        To get started, you can  <strong>"Let’s get started"</strong> or upload a document below.
+        To get started, you can  press <strong>"Let’s get started"</strong> 
         <br />
         
         <br />
         <em>
           “Just to be clear, could you let me know what you'd like to do today?<br />
-          – Review an existing OKR<br />
+          – Review an existing OKR - upload or copy/paste below<br />
           – Develop a new OKR with my help<br />
           – Ask a specific question about OKRs or logic modelling?”
         </em>
